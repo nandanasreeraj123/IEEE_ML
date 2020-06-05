eabae9cab15a9c04f2fcfc06441b65059483bf0c
Nandana <nandanasreeraj@gmail.com>
2020-06-05 19:40:23 +0530

    day4

--- Day4/t4.py
+++ Day4/t4.py
@@ -2,26 +2,24 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 data = pd.read_csv("http://iali.in/datasets/IEEEAPSIT/unsupervised-ml/Wholesale%20customers%20data.csv")
+
 #to see if we are correct
 indices = [14,74,228,300,310]
 
-samples = pd.DataFrame(data.loc[indices], columns=data.keys()).reset_index(drop=True)
-log_data = np.log(data.copy())
-log_samples = np.log(samples)
-print("Sample:")
-print(samples)
-print("Log-transformed samples:")
-print(log_samples)
-for feature in log_data.keys():
-    Q1 = np.percentile(log_data, 25)
-    Q3 = np.percentile(log_data, 75)
+ind = pd.DataFrame(data.loc[indices], columns=data.keys()).reset_index(drop=True)
+cov_to_log = np.log(data.copy())
+converted_samples = np.log(ind)
+
+for every in cov_to_log.keys():
+    Q1 = np.percentile(cov_to_log, 25)
+    Q3 = np.percentile(cov_to_log, 75)
     step = (Q3 - Q1) * 1.5
-    print("Data points considered outliers for the feature '{}':".format(feature))
-    print(log_data[~((log_data[feature] >= Q1 - step) & (log_data[feature] <= Q3 + step))])
+    print("outliners '{}':".format(every))
+    print(cov_to_log[~((cov_to_log[every] >= Q1 - step) & (cov_to_log[every] <= Q3 + step))])
 
 pca = PCA(n_components=6)
-pca.fit(log_data)
-pca_samples = pca.transform(log_samples)
+pca.fit(cov_to_log)
+pca_samples = pca.transform(converted_samples)
 print(pca.components_)
 print(pca.explained_variance_)
 print(pca_samples)
